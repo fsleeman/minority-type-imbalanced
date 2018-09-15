ecaee032c1408a6fe185a2cfb210c2752bcfaf83
Ford Sleeman <fsleeman@gmail.com>
2018-09-15 17:57:25 -0400

    Added run script

---
+++ scripts/run_spark_jobs.py
@@ -0,0 +1,16 @@
+
+
+def main():
+    resampling_methods = ['none', 'undersample', 'oversample', 'smote', 'smotePlus']
+    k_value = 5
+    output_directory = '/home/ford/data/poker/results'
+    rw = 'write'
+    rw_path = '/home/ford/data/poker/results/minorityDf10k'
+
+
+    print()
+
+
+
+if __name__ == "__main__":
+    main()
